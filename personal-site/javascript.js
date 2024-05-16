49be4adc84e8e6fa94f6ab18b589a18f09c5bf4e
Isaiah-o <167384925+Isaiah-o@users.noreply.github.com>
2024-05-15 20:26:48 -0700

    Add files via upload

--- personal-site/javascript.js
+++ personal-site/javascript.js
@@ -1,2 +1,14 @@
 console.log('howdy world!')
 
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+    submitButton.disabled = true
+    e.preventDefault()
+    window.location.href = window.location.origin + '/success.html'
+  })
+
+
+
